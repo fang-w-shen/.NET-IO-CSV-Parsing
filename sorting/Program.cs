dbb0214dff0b3603bebdcc604611a8ac05925a7a
Fang <fang-wen.shen@vanderbilt.edu>
2018-01-27 19:38:52 -0600

    explore hashset, dictionary in generic collections, built morse code to text and text to morse code translator

---
+++ sorting/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Treehouse
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<Student> students = new HashSet<Student>
+            {
+                new Student() {Name = "Sally", GradeLevel = 3},
+                new Student() {Name = "Bob", GradeLevel = 3},
+                new Student() {Name = "Sally", GradeLevel = 2}
+            };
+            Student joe = new Student() { Name = "Joe", GradeLevel = 2 };
+            students.Add(joe);
+            Student duplicatejoe = new Student() { Name = "Joe", GradeLevel = 2 };
+            Console.WriteLine(joe.Equals(duplicatejoe));
+            foreach (Student student in students)
+            {
+                Console.WriteLine(string.Format("{0} is in grade {1}", student.Name, student.GradeLevel));
+            }
+
+            if (students.Contains(duplicatejoe))
+            {
+                Console.WriteLine("does");
+         
+            }
+            else
+            {
+                Console.WriteLine("does not");
+            }
+            //students.Sort();
+            //Student b = students[0];
+            //int index = students.IndexOf(b);
+            //if(index < 0)
+            //{
+            //    students.Insert(~index, b);
+            //}
+            //Console.WriteLine(index);
+
+            //SchoolRoll schoolRoll = new SchoolRoll();
+            // schoolRoll.AddStudents(students);
+            // because <List> was changed to <IEnumerable> now dont have removeat sort or addrange methods
+            //schoolRoll.Students.RemoveAt(0);
+            //schoolRoll.Students.Sort();
+            //schoolRoll.Students.AddRange(students);
+
+        }
+    }
+}
---
+++ sorting/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Treehouse
+{
+    class Student : IComparable<Student>
+    {
+        public string Name { get; set; }
+        public int GradeLevel { get; set; }
+        public int CompareTo(Student that)
+        {
+            int result = this.Name.CompareTo(that.Name);
+            if (result == 0)
+            {
+                result = this.GradeLevel.CompareTo(that.GradeLevel);
+
+            }
+            return result;
+        }
+        public override int GetHashCode()
+        {
+            var n = Name.GetHashCode() + GradeLevel.GetHashCode();
+            return n.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            
+            if (!(obj is Student))
+            {
+                return false;
+            }
+            Student that = obj as Student;
+            return this.Name == that.Name && this.GradeLevel == that.GradeLevel;
+        }
+    }
+}
